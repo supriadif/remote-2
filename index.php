d4a1f14c8aabbfb948b27e8ce3eba68fed2a83b3
supriadif <kemensenbud.bemums@gmail.com>
2021-01-05 20:05:57 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar remote git dan github 2</title>
+</head>
+<body>
+    
+    <h1>Remote git dan github 2</h1>
+    
+</body>
+</html>
