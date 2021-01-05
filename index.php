291547026c0d8324c05e30cdc41e178cc775d79d
supriadif <kemensenbud.bemums@gmail.com>
2021-01-05 20:13:26 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -11,5 +11,9 @@
 
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis delectus quam quos nihil doloremque natus omnis! Pariatur dicta nulla recusandae expedita perferendis. Doloremque veritatis voluptatum nostrum ipsam architecto laboriosam autem?</p>
 
+    <ul>
+        <li>L200160149</li>
+    </ul>
+
 </body>
 </html>
